7c31f97153919290d11c77ac2e281d35fdf6bef4
TravisT <travis.don.tillotson@gmail.com>
2018-03-14 16:43:11 -0700

    Build Basic ControllerBase

--- skeleton 11/bin/p01_basic_server.rb
+++ skeleton 11/bin/p01_basic_server.rb
@@ -4,7 +4,7 @@ app = Proc.new do |env|
   req = Rack::Request.new(env)
   res = Rack::Response.new
   res['Content-type'] = 'text/html'
-  res.write('Here\'s a response')
+  res.write("<p>#{env['REQUEST_PATH']}</p>")
   res.finish
 end
 
--- skeleton 11/lib/controller_base.rb
+++ skeleton 11/lib/controller_base.rb
@@ -8,20 +8,36 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req, @res = req, res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if already_built_response?
+      raise
+    else
+      @already_built_response = true
+      res.set_header('location', "#{url}")
+      res.status = 302
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    if already_built_response?
+      raise
+    else
+      @already_built_response = true
+      res.body = [content]
+      res["Content-Type"] = "#{content_type}"
+    end
   end
 
   # use ERB and binding to evaluate templates
@@ -37,4 +53,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
